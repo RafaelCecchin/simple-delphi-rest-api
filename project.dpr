97da0a34f6089c5133f308eb64427c2bbac176af
Rafael <contatorafaelcecchin@gmail.com>
2022-04-11 22:27:28 -0300

    feature: adicionar GET e POST e salvar em memória

--- project.dpr
+++ project.dpr
@@ -4,19 +4,41 @@ program project;
 
 {$R *.res}
 
-uses Horse, System.JSON;
+uses
+  Horse,
+  Horse.Jhonson,
+  System.JSON;
 
 var
   App: THorse;
+  Persons: TJsonArray;
 
 begin
-
   App := THorse.Create();
+  Persons := TJsonArray.Create();
+
+  App.Use(Jhonson);
 
   App.Get('/ping',
     procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
     begin
       Res.Send('pong');
+    end);
+
+  App.Get('/persons',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send<TJSONAncestor>(Persons.Clone);
+    end);
+
+  App.Post('/person',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      Person: TJSONObject;
+    begin
+      Person := Req.Body<TJSONObject>.Clone as TJSONObject;
+      Persons.AddElement(Person);
+      Res.Send<TJSONAncestor>(Person.Clone).Status(201);
     end);
 
   App.Listen(9000);
